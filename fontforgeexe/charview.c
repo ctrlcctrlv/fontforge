8143050915e997e7042b3981bc3f51832a122f28
repu1sion <bearrailgun@gmail.com>
2019-08-10 23:53:03 +0300

    renamed all C++ reserved words in fontforgeexe/

--- fontforgeexe/charview.c
+++ fontforgeexe/charview.c
@@ -2062,7 +2062,7 @@ static void CVShowHints(CharView *cv, GWindow pixmap) {
     DStemInfo *dstem;
     MinimumDistance *md;
     char *blues, *others;
-    struct psdict *private = cv->b.sc->parent->private;
+    struct psdict *private = cv->b.sc->parent->private_;
     char buf[20];
     int len, len2;
     SplinePoint *sp;
@@ -6899,7 +6899,7 @@ static void fllistcheck(GWindow gw, struct gmenuitem *mi,GEvent *UNUSED(e)) {
 	    mi->ti.disabled = fvs==NULL || cv->b.container!=NULL;
 	  break;
 	  case MID_Revert:
-	    mi->ti.disabled = cv->b.fv->sf->origname==NULL || cv->b.fv->sf->new || cv->b.container;
+	    mi->ti.disabled = cv->b.fv->sf->origname==NULL || cv->b.fv->sf->new_ || cv->b.container;
 	  break;
 	  case MID_RevertGlyph:
 	    mi->ti.disabled = cv->b.fv->sf->filename==NULL ||
